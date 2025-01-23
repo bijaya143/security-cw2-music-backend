fe46578b245f2b87acacc61709a311b3c46c46ba
bijaya143 <nemomajhi14@gmail.com>
2025-01-23 19:24:29 +0545

    ManagePoint API URL Setup

--- src/service/smsService.js
+++ src/service/smsService.js
@@ -2,7 +2,7 @@ const config = require("../config");
 
 // Multi factor auth implementation
 async function sendSMS(to, message) {
-  const url = "https://api.managepoint.co/api/sms/send";
+  const url = "https://api.managepoint.co/api/sms/send"; // ManagePoint API URL
   const apiKey = config.SMS_API_KEY;
   const payload = {
     apiKey,
